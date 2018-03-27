df113b4e332055f3231c9852ab4840fba64f7134
Joseph Rusk <josephraylee@gmail.com>
2018-03-26 20:21:18 -0700

    pg 36 commit

---
+++ NewCSharpCourse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NewCSharpCourse
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Math math = new Math();
+                math.Addition();
+                math.Division();
+                math.Multiplication();
+                Console.Read();
+
+            }
+            catch{
+                Console.WriteLine("Please enter a vaid number.");
+
+            }
+        }
+    }
+}
---
+++ NewCSharpCourse/math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace NewCSharpCourse
+{
+    public class Math
+    {
+        public void Addition()
+        {
+            Console.WriteLine("Type a number too add to 10");
+            int add = Convert.ToInt32(Console.ReadLine());
+            int total = add + 10;
+            Console.WriteLine(total);
+        }
+        public void Multiplication()
+        {
+            Console.WriteLine("Type a number to multiply 30 by.");
+            int mult = Convert.ToInt32(Console.ReadLine());
+            double total = mult * 30;
+            Console.WriteLine(total);
+        }
+        public void Division()
+        {
+            Console.WriteLine("Type a number to divide 304309 by.");
+            int div = Convert.ToInt32(Console.ReadLine());
+            int total = 304309/div;
+            Console.WriteLine(total);
+        }
+
+
+
+    }
+}
+
