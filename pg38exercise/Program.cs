a4d64d6eea9e78b8fd0f4efd66b0e7b8cecb6357
Joseph Rusk <josephraylee@gmail.com>
2018-03-27 18:51:44 -0700

    pg: 38

---
+++ pg38exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pg38exercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a number.");
+            math.UserInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In the Main() method of the console app, instantiate the class and call the one method, passing in an integer. 3. Display the result to the screen.");
+            Console.WriteLine(math.ThisThing());
+            Console.WriteLine("Add a second method to the class, with the same name, that will take in a decimal, do a different math operation to it and then return the answer as an integer./nIn the Main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the result to the screen");
+            Console.WriteLine(math.ThisThing(3.2));
+            Console.WriteLine("Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it and then return the answer as an integer. In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer.Display the result to the screen.");
+            Console.WriteLine(math.ThisThing(math.UserInput,5));
+        }
+    }
+}
---
+++ pg38exercise/math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace pg38exercise
+{
+    public class Math
+    {
+        public int UserInput { get; set; }
+        public int Answer { get; set; }
+        public double DAnswer { get; set; }
+
+        public int ThisThing()
+        {           
+           return Answer = UserInput + 35;
+        }
+        public double ThisThing(double times)
+        {
+            return DAnswer = UserInput * (2 * times);
+
+        }
+        public int ThisThing(int div, int mult)
+        {
+            return Answer = UserInput * (100 / div)* mult;
+        }
+    }
+}
