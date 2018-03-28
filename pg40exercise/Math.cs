677c54ee83c1ef441d7ad92d96f85633e98cf596
Joseph Rusk <josephraylee@gmail.com>
2018-03-27 20:25:20 -0700

    pg40

---
+++ pg40exercise/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace pg40exercise
+{
+    public class Math
+    {
+       
+        public void Method(int firstInput, int secondInput)
+        {
+            int multnum = secondInput;
+            int firstanswer = firstInput * multnum;
+            Console.WriteLine(firstInput + " multiplied by " + multnum + " is " + firstanswer + ".");
+            Console.WriteLine("firstInput= " + firstInput + "\nsecondInput= " + secondInput);
+            return;
+        }
+    }
+}
---
+++ pg40exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pg40exercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Enter your first integer");
+            int firstInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second integer.");
+            int secondInput = Convert.ToInt32(Console.ReadLine());
+
+            math.Method(firstInput, secondInput);
+
+        }
+    }
+}
