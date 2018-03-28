617ce39f3e5304539c14a470d302121a15584673
Joseph Rusk <josephraylee@gmail.com>
2018-03-27 19:28:33 -0700

    pg 39

---
+++ pg39exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace pg39exercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a number.");
+            math.UserInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number... if you feel like it.");
+            try
+            {
+                math.OptUserInput = Convert.ToInt32(Console.ReadLine());
+            }
+            catch{
+                math.OptUserInput = 0;
+            }
+
+            if (math.OptUserInput > 0){
+                Console.WriteLine(math.UserInput + " + " + "is equal to: " + math.mathOperation());
+            }
+            else Console.WriteLine("You typed " + math.UserInput);
+        }
+    }
+}
---
+++ pg39exercise/math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace pg39exercise
+{
+    public class Math
+    {
+        public int UserInput { get; set; }
+        public int OptUserInput { get; set; }
+        public int Answer { get; set; }
+
+        public int mathOperation()
+        {
+            return Answer = UserInput + OptUserInput;
+        }
+    }
+}
