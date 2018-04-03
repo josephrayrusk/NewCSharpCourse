32c77c6f95b29f918d4883111745d5d0b8406b81
Joseph Rusk <josephraylee@gmail.com>
2018-04-02 19:06:41 -0700

    updatepg48

--- pg48exercise/Person.cs
+++ pg48exercise/Person.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace pg48exercise
 {
-    public class Person
+    public abstract class Person
     {
         public string FirstName { get;set;  }
         public string LastName { get; set; }
