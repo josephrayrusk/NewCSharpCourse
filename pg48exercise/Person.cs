091dbddfbdd781897ef0e5818134e26d50ebba81
Joseph Rusk <josephraylee@gmail.com>
2018-04-02 19:16:18 -0700

    Another Update

--- pg48exercise/Person.cs
+++ pg48exercise/Person.cs
@@ -5,5 +5,10 @@ namespace pg48exercise
     {
         public string FirstName { get;set;  }
         public string LastName { get; set; }
+
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName + ".");
+        }
     }
 }
