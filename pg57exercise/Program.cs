916632b24234f3d28d31f8099421b12ba1ad4d20
Joseph Rusk <josephraylee@gmail.com>
2018-04-03 20:53:58 -0700

    pg 57

---
+++ pg57exercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pg57exercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week");
+            string today = Console.ReadLine();
+            try
+            {
+                if (Enum.IsDefined(typeof(DaysOfTheWeek), today))
+                {
+                    Console.WriteLine("Yay");
+
+                }
+                else throw new Exception();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a actual day of the week.");
+            }
+           
+        }
+        public enum DaysOfTheWeek
+        {
+            Monday=1,
+            Tuesday=2,
+            Wednesday=3,
+            Thursday=4,
+            Friday=5,
+            Saturday=6,
+            Sunday=7
+        }
+    }
+}
