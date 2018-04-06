e60e9a34f1d8487675336b3f958d15dcf1428b96
Joseph Rusk <josephraylee@gmail.com>
2018-04-05 18:06:41 -0700

    pg59

---
+++ pg59exercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pg59exercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 34;
+            Console.WriteLine(number.Amount);
+            Console.Read();
+        }
+    }
+}
