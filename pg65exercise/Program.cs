4e402a9b218b9f6b85955db85d6bf6483af8dbf9
Joseph Rusk <josephraylee@gmail.com>
2018-04-08 17:20:39 -0700

    pg65

---
+++ pg65exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+namespace pg65exercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            //using string just for exercise purposes only.
+            string number = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Joseph\Log\testlog.txt", number);
+            string readNumber = File.ReadAllText(@"C:\Users\Joseph\Log\testlog.txt");
+            Console.WriteLine(readNumber);
+        }
+    }
+}
