766ddf00b4c55ca0f3df03edd0b7dcf797d5dbe7
Joseph Rusk <josephraylee@gmail.com>
2018-04-08 17:48:33 -0700

    pg67

---
+++ pg67exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pg67exercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please type a number.");
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(DateTime.Now.AddHours(number));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadLine();
+        }
+    }
+}
